eae79820f14159059b25759c1855f33e13ef4f2a
Vaynbaum <78900834+Vaynbaum@users.noreply.github.com>
2021-03-28 11:15:53 +0300

    Вайнбаум

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,16 +12,16 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int count = 0, left = 0, right=len-1;
-    while (left < right - 1){
+  int count = 0, left = 0, right = len - 1;
+    while (left < right - 1) {
         int middle = (left + right) / 2;
         if (arr[middle] <= value)
-            left = middle; 
+            left = middle;
         else
             right = middle;
     }
     len = right-1;
-  
+
     for (int i = len; i >=0; i--) {
         for (int j = 0; j < i; j++) {
             if (arr[i] + arr[j] == value)
@@ -43,7 +43,7 @@ int countPairs3(int *arr, int len, int value) {
             right = middle;
     }
     len = right - 1;
-    
+
     for (int i = 0; i <len; i++) {
         left = i+1, right = len-1;
         int countNum = 0;
